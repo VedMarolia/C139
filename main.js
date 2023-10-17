8c18d63b8f64425faf0251862c8758a0070a1c9f
VedMarolia <118649989+VedMarolia@users.noreply.github.com>
2023-10-17 17:26:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+noseX = ""
+noseY = ""
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas")
+
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO)
+	video.size(800 , 400)
+	video.parent("game_console")
+
+	poseNet = ml5.poseNet(video , modelLoaded)
+	poseNet.on("pose" , gotResults)
+}
+
+function draw() {
+	game()
+}
+
+function modelLoaded(){
+	console.log("Model is Loaded")
+}
+
+function gotResults(results){
+	if(results.lenght > 0){
+		console.log(results)
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
